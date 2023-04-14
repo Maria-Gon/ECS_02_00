2bd84e4d3eb7234ce3556b73ffdf0c093c0919d6
m.gonzaleze <69689894+Maria-Gon@users.noreply.github.com>
2023-04-14 08:02:12 -0500

    Tutorial

--- src/create/prefab_creator.py
+++ src/create/prefab_creator.py
@@ -2,13 +2,16 @@ import random
 import pygame
 import esper
 from src.ecs.components.c_enemy_spawner import CEnemySpawner
+from src.ecs.components.c_input_command import CInputCommand
 
 from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
 from src.ecs.components.c_velocity import CVelocity
+from src.ecs.components.tags.c_tag_enemy import CTagEnemy
+from src.ecs.components.tags.c_tag_player import CTagPlayer
 
 def create_square(world:esper.World, size:pygame.Vector2,
-                    pos:pygame.Vector2, vel:pygame.Vector2, col:pygame.Color):
+                    pos:pygame.Vector2, vel:pygame.Vector2, col:pygame.Color) -> int:
     cuad_entity = world.create_entity()
     world.add_component(cuad_entity,
                 CSurface(size, col))
@@ -16,6 +19,7 @@ def create_square(world:esper.World, size:pygame.Vector2,
                 CTransform(pos))
     world.add_component(cuad_entity, 
                 CVelocity(vel))
+    return cuad_entity
 
 def create_enemy_square(world:esper.World, pos:pygame.Vector2, enemy_info:dict):
     size = pygame.Vector2(enemy_info["size"]["x"], 
@@ -28,9 +32,34 @@ def create_enemy_square(world:esper.World, pos:pygame.Vector2, enemy_info:dict):
     vel_range = random.randrange(vel_min, vel_max)
     velocity = pygame.Vector2(random.choice([-vel_range, vel_range]),
                               random.choice([-vel_range, vel_range]))
-    create_square(world, size, pos, velocity, color)
+    enemy_entity = create_square(world, size, pos, velocity, color)
+    world.add_component(enemy_entity, CTagEnemy())
+
+def create_player_square(world: esper.World, player_info: dict, player_lvl_info: dict) -> int:
+    size = pygame.Vector2(player_info['size']['x'],
+                          player_info['size']['y'])
+    color = pygame.Color(player_info['color']['r'],
+                         player_info['color']['g'],
+                         player_info['color']['b'])
+    pos = pygame.Vector2(player_lvl_info['position']['x'] - size.x/2,
+                         player_lvl_info['position']['y'] - size.y/2)
+    vel = pygame.Vector2(0,0)
+
+    player_entity = create_square(world, size, pos, vel, color)
+    world.add_component(player_entity, CTagPlayer())
+    return player_entity
 
 def create_enemy_spawner(world:esper.World, level_data:dict):
     spawner_entity = world.create_entity()
     world.add_component(spawner_entity,
                         CEnemySpawner(level_data["enemy_spawn_events"]))
+    
+def create_input_player (world: esper.World):
+    input_left = world.create_entity()
+    nput_right = world.create_entity()
+    nput_up = world.create_entity()
+    nput_down = world.create_entity()
+    world.add_component(input_left, CInputCommand('PLAYER_LEFT', pygame.K_LEFT))
+    world.add_component(nput_right, CInputCommand('PLAYER_RIGHT', pygame.K_RIGHT))
+    world.add_component(nput_up, CInputCommand('PLAYER_UP', pygame.K_UP))
+    world.add_component(nput_down, CInputCommand('PLAYER_DOWN', pygame.K_DOWN))
--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -1,13 +1,17 @@
 import json
 import pygame
 import esper
+from src.ecs.components.c_input_command import CInputCommand, CommandPhase
+from src.ecs.components.c_velocity import CVelocity
+from src.ecs.systems.s_collision_player_enemy import system_collision_player_enemy
 from src.ecs.systems.s_enemy_spawner import system_enemy_spawner
+from src.ecs.systems.s_input_player import system_input_player
 
 from src.ecs.systems.s_movement import system_movement
 from src.ecs.systems.s_rendering import system_rendering
 from src.ecs.systems.s_screen_bounce import system_screen_bounce
 
-from src.create.prefab_creator import create_enemy_spawner
+from src.create.prefab_creator import create_enemy_spawner, create_input_player, create_player_square
 
 class GameEngine:
     def __init__(self) -> None:
@@ -35,6 +39,8 @@ class GameEngine:
             self.enemies_cfg = json.load(enemies_file)
         with open("assets/cfg/level_01.json") as level_01_file:
             self.level_01_cfg = json.load(level_01_file)
+        with open("assets/cfg/player.json") as player_file:
+            self.player_cfg = json.load(player_file)
 
     def run(self) -> None:
         self._create()
@@ -47,8 +53,10 @@ class GameEngine:
         self._clean()
 
     def _create(self):
+        self._player_entity = create_player_square(self.ecs_world, self.player_cfg, self.level_01_cfg['player_spawn'])
+        self._player_c_v = self.ecs_world.component_for_entity(self._player_entity, CVelocity)
         create_enemy_spawner(self.ecs_world, self.level_01_cfg)
-        pass
+        create_input_player(self.ecs_world)
 
     def _calculate_time(self):
         self.clock.tick(self.framerate)
@@ -56,6 +64,7 @@ class GameEngine:
     
     def _process_events(self):
         for event in pygame.event.get():
+            system_input_player(self.ecs_world, event, self._do_action)
             if event.type == pygame.QUIT:
                 self.is_running = False
 
@@ -63,6 +72,8 @@ class GameEngine:
         system_enemy_spawner(self.ecs_world, self.enemies_cfg, self.delta_time)
         system_movement(self.ecs_world, self.delta_time)
         system_screen_bounce(self.ecs_world, self.screen)
+        system_collision_player_enemy(self.ecs_world, self._player_entity, self.level_01_cfg)
+        self.ecs_world._clear_dead_entities()
 
     def _draw(self):
         self.screen.fill(self.bg_color)
@@ -70,6 +81,28 @@ class GameEngine:
         pygame.display.flip()
 
     def _clean(self):
+        self.ecs_world.clear_database()
         pygame.quit()
 
+    def _do_action(self, c_input: CInputCommand):
+        if c_input.name == 'PLAYER_LEFT':
+            if c_input.phase == CommandPhase.START:
+                self._player_c_v.vel.x -= self.player_cfg['input_velocity']
+            elif c_input.phase == CommandPhase.END:
+                self._player_c_v.vel.x += self.player_cfg['input_velocity']
+        if c_input.name == 'PLAYER_RIGHT':
+            if c_input.phase == CommandPhase.START:
+                self._player_c_v.vel.x += self.player_cfg['input_velocity']
+            elif c_input.phase == CommandPhase.END:
+                self._player_c_v.vel.x -= self.player_cfg['input_velocity']
+        if c_input.name == 'PLAYER_UP':
+            if c_input.phase == CommandPhase.START:
+                self._player_c_v.vel.y -= self.player_cfg['input_velocity']
+            elif c_input.phase == CommandPhase.END:
+                self._player_c_v.vel.y += self.player_cfg['input_velocity']
+        if c_input.name == 'PLAYER_DOWN':
+            if c_input.phase == CommandPhase.START:
+                self._player_c_v.vel.y += self.player_cfg['input_velocity']
+            elif c_input.phase == CommandPhase.END:
+                self._player_c_v.vel.y -= self.player_cfg['input_velocity'] 
     
